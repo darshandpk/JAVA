768427e1e58d41d158722ff72b7b3aa5c9340c15
darshandpk <darshankalmengh17@gmail.com>
2025-02-07 00:06:10 +0530

    Resolve the Errors
    In this Java File I Resolve some Errors like adding ' ; ' and creating the object of the Scanner class to take the input from the user.

--- Programing/CondOperExample.java
+++ Programing/CondOperExample.java
@@ -1,17 +1,20 @@
 import java.util.Scanner;
-class CondOperExample{
-	public static void main(String[]args){
+
+class CondOperExample {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
 
 		System.out.println("Enter the 1 Number: ");
 		int a = sc.nextInt();
 		System.out.println("Enter the 1 Number: ");
-		int b = sc.nextInt()
+		int b = sc.nextInt();
 		System.out.println("Enter the 1 Number: ");
 		int c = sc.nextInt();
 
-		int lar = (a>b)?((a>c)?(a):(c)):((b>c)?(b):(c));
+		int lar = (a > b) ? ((a > c) ? (a) : (c)) : ((b > c) ? (b) : (c));
 
-		System.out.println("Largest Of three Number is "+ lar);
+		System.out.println("Largest Of three Number is " + lar);
 
 	}
 
