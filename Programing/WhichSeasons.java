74dc1d8f8aecda4c688403613896a635f80600cc
darshandpk <darshankalmengh17@gmail.com>
2025-02-07 00:14:26 +0530

    Resolving Errors
    Did Some Change to these Programs For that I had Left By Mistake.

--- Programing/WhichSeasons.java
+++ Programing/WhichSeasons.java
@@ -1,18 +1,22 @@
 import java.util.Scanner;
 
-class WhichSeasons{
-	public static void main(String[]args){
+class WhichSeasons {
+	public static void main(String[] args) {
 
-		Scanner sc=new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 
 		System.out.println("Enter the Month: ");
-		String month = sc.next().UpperCase();
+		String month = sc.next().toUpperCase();
 
-		String result = 
-				((month.equals("OCT")) || (month.equals("NOV")) || (month.equals("DEC")) || (month.equals("JAN")))?
-				(month+"is WINTER"):(((month.equals("FEB")) || (month.equals("MAR")) || (month.equals("APR"))|| (month.equals("MAY")))?
-				(month"is SUMMER"):(((month.equals("JUN")) || (month.equals("JUL")) || (month.equals("AUG")) || (month.equals("SEP")))?
-				(month"is MONSOON"): (INVALID MONTH));
+		String result = ((month.equals("OCT")) || (month.equals("NOV")) || (month.equals("DEC"))
+				|| (month.equals("JAN")))
+						? (month + "is WINTER")
+						: (((month.equals("FEB")) || (month.equals("MAR")) || (month.equals("APR"))
+								|| (month.equals("MAY")))
+										? (month + "is SUMMER")
+										: (((month.equals("JUN")) || (month.equals("JUL")) || (month.equals("AUG"))
+												|| (month.equals("SEP"))) ? (month + "is MONSOON")
+														: ("INVALID MONTH")));
 
 		System.out.println(result);
 	}
