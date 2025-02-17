b8a3a8b4d4db6200508e9749066f792b30d4bbe1
darshandpk <darshankalmengh17@gmail.com>
2025-02-18 01:12:45 +0530

    AlphabetSeries
    I print the A C F J O U using the java Program.

---
+++ Programing/AlphabetSeries.java
@@ -0,0 +1,13 @@
+class AlphabetSeries {
+	public static void main(String[] args) {
+
+		int a = 1;
+		for (char ch = 'A'; ch <= 'Z'; ch += a) {
+			System.out.print(" " + ch);
+			a++;
+
+		}
+
+	}
+
+}
